c147ab88f07d0c8da2a9d0d325636e203252032f
Tiago Weber <tiagowebersc@gmail.com>
2019-10-12 06:07:05 +0200

    starting super admin layout

---
+++ CommunalIntegration/resources/views/town_manager.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.super_admin_template')
+
+@section('title', '4 Motion - Towns')
+
+@section('css')
+<link href="https://fonts.googleapis.com/css?family=Montserrat:600&display=swap" rel="stylesheet">
+@endsection
+
+@section('content')
+
+<main>
+
+    <h1>Test</h1>
+
+
+</main>
+
+@endsection
--- CommunalIntegration/routes/web.php
+++ CommunalIntegration/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/superAdmin', function () {
+    return view('town_manager');
+});
