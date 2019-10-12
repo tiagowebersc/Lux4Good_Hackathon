ed1bcc518274f3c3a3b256a6957e2221df34a7c8
Tiago Weber <tiagowebersc@gmail.com>
2019-10-12 11:43:13 +0200

    seeder outcome

--- CommunalIntegration/database/seeds/OutcomeTableSeeder.php
+++ CommunalIntegration/database/seeds/OutcomeTableSeeder.php
@@ -11,88 +11,20 @@ class OutcomeTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('outcome')->insert([
-            'action_id' => 1,
-            'age' => rand(0, 5),
-            'income' => rand(0, 8),
-            'stay_duration_country' => rand(0, 8),
-            'stay_duration_town' => rand(0, 8),
-            'child' => rand(0, 4),
-            'country_of_birth_id' => 1,
-            'gender' => rand(0, 1),
-            'education' => rand(0, 5),
-            'marital_status' => rand(0, 5),
-            'professional_status' => rand(0, 3)
-        ]);
-
-        DB::table('outcome')->insert([
-            'action_id' => 2,
-            'age' => rand(0, 5),
-            'income' => rand(0, 8),
-            'stay_duration_country' => rand(0, 8),
-            'stay_duration_town' => rand(0, 8),
-            'child' => rand(0, 4),
-            'country_of_birth_id' => 2,
-            'gender' => rand(0, 1),
-            'education' => rand(0, 5),
-            'marital_status' => rand(0, 5),
-            'professional_status' => rand(0, 3)
-        ]);
-
-        DB::table('outcome')->insert([
-            'action_id' => 3,
-            'age' => rand(0, 5),
-            'income' => rand(0, 8),
-            'stay_duration_country' => rand(0, 8),
-            'stay_duration_town' => rand(0, 8),
-            'child' => rand(0, 4),
-            'country_of_birth_id' => 3,
-            'gender' => rand(0, 1),
-            'education' => rand(0, 5),
-            'marital_status' => rand(0, 5),
-            'professional_status' => rand(0, 3)
-        ]);
-
-        DB::table('outcome')->insert([
-            'action_id' => 4,
-            'age' => rand(0, 5),
-            'income' => rand(0, 8),
-            'stay_duration_country' => rand(0, 8),
-            'stay_duration_town' => rand(0, 8),
-            'child' => rand(0, 4),
-            'country_of_birth_id' => 4,
-            'gender' => rand(0, 1),
-            'education' => rand(0, 5),
-            'marital_status' => rand(0, 5),
-            'professional_status' => rand(0, 3)
-        ]);
-
-        DB::table('outcome')->insert([
-            'action_id' => 5,
-            'age' => rand(0, 5),
-            'income' => rand(0, 8),
-            'stay_duration_country' => rand(0, 8),
-            'stay_duration_town' => rand(0, 8),
-            'child' => rand(0, 4),
-            'country_of_birth_id' => 5,
-            'gender' => rand(0, 1),
-            'education' => rand(0, 5),
-            'marital_status' => rand(0, 5),
-            'professional_status' => rand(0, 3)
-        ]);
-
-        DB::table('outcome')->insert([
-            'action_id' => 6,
-            'age' => rand(0, 5),
-            'income' => rand(0, 8),
-            'stay_duration_country' => rand(0, 8),
-            'stay_duration_town' => rand(0, 8),
-            'child' => rand(0, 4),
-            'country_of_birth_id' => 6,
-            'gender' => rand(0, 1),
-            'education' => rand(0, 5),
-            'marital_status' => rand(0, 5),
-            'professional_status' => rand(0, 3)
-        ]);
+        for ($i=1; $i < 10000; $i++) { 
+            DB::table('outcome')->insert([
+                'action_id' => rand(1,7),
+                'age' => rand(0, 5),
+                'income' => rand(0, 8),
+                'stay_duration_country' => rand(1968,2014),
+                'stay_duration_town' => rand(1968,2014),
+                'child' => rand(0, 4),
+                'country_of_birth_id' => 1,
+                'gender' => rand(0, 1),
+                'education' => rand(0, 5),
+                'marital_status' => rand(0, 5),
+                'professional_status' => rand(0, 3)
+            ]);
+        }
     }
 }
