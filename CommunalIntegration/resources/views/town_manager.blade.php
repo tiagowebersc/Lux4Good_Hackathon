6dd07b04fccdf53caf9d82cd94a353d2fe326b53
Joao Felicio <joaolfelicio@gmail.com>
2019-10-12 08:20:39 +0200

    working on super admin page

--- CommunalIntegration/resources/views/town_manager.blade.php
+++ CommunalIntegration/resources/views/town_manager.blade.php
@@ -4,13 +4,41 @@
 
 @section('css')
 <link href="https://fonts.googleapis.com/css?family=Montserrat:600&display=swap" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="{{URL::asset('/css/town_manager.css')}}" />
 @endsection
 
 @section('content')
 
-<main>
+<main class='mainList'>
 
-    <h1>Test</h1>
+    <div class="listHeader">
+        <h1>TOWN</h1>
+
+        <div class="form-group has-search">
+            <span class="fa fa-search form-control-feedback"></span>
+            <input type="text" class="form-control" placeholder="Search">
+        </div>
+
+        <input class='addButton' value='ADD TOWN +' type="submit">
+    </div>
+
+    <div class="listContent">
+
+        @for ($i = 0; $i < 8; $i++)
+
+        <div class="listContentItem">
+
+            <p class='itemTitle'>Petange</p>
+            <img src="{{URL::asset('images/beautiful.jpeg')}}" alt="image of 'insert name'">
+
+        </div>
+
+        @endfor
+
+    </div>
+
+    <div class="listPagination"></div>
 
 
 </main>
