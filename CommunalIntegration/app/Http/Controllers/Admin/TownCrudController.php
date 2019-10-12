24e5e6def67120005efd473474621b07a8386d9e
Gökdağ Göktepe <gokdag.goktepe@gmail.com>
2019-10-12 11:58:37 +0200

    TownCrudController

--- CommunalIntegration/app/Http/Controllers/Admin/TownCrudController.php
+++ CommunalIntegration/app/Http/Controllers/Admin/TownCrudController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Requests\TownRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use Auth;
 
 /**
  * Class TownCrudController
@@ -29,7 +30,8 @@ class TownCrudController extends CrudController
     protected function setupListOperation()
     {
         // TODO: remove setFromDb() and manually define Columns, maybe Filters
-        $this->crud->setFromDb();
+        //$this->crud->setFromDb();
+        $this->crud->addColumn(['name' => 'name', 'type' => 'text', 'label' => 'Name']);
     }
 
     protected function setupCreateOperation()
@@ -37,11 +39,22 @@ class TownCrudController extends CrudController
         $this->crud->setValidation(TownRequest::class);
 
         // TODO: remove setFromDb() and manually define Fields
-        $this->crud->setFromDb();
+        //$this->crud->setFromDb();
+        $this->crud->addField(['name' => 'name', 'type' => 'text', 'label' => 'Name']);
+        // Hidden field relate to : created_by / updated_by
+        $this->crud->addField(['name' => 'created_by', 'type' => 'hidden', 'label' => 'Created By', 'value'=> backpack_user()->id]);
+        $this->crud->addField(['name' => 'updated_by', 'type' => 'hidden', 'label' => 'Updated By', 'value'=> backpack_user()->id]);
     }
 
     protected function setupUpdateOperation()
     {
-        $this->setupCreateOperation();
+        $this->crud->setValidation(TownRequest::class);
+
+        // TODO: remove setFromDb() and manually define Fields
+        //$this->crud->setFromDb();
+        $this->crud->addField(['name' => 'name', 'type' => 'text', 'label' => 'Name']);
+        // Hidden field relate to : created_by / updated_by
+        $this->crud->addField(['name' => 'updated_by', 'type' => 'hidden', 'label' => 'Updated By', 'value'=> backpack_user()->id]);
+    
     }
 }
